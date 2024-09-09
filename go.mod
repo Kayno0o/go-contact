609c26040563d1c43e38f626ddfd1ab9b32dea37
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-09-09 19:45:25 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,45 @@
+module kevyn.fr/contact
+
+go 1.23.0
+
+require (
+	github.com/dchest/captcha v1.0.0
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/uptrace/bun v1.2.3
+	github.com/uptrace/bun/dialect/sqlitedialect v1.2.3
+	github.com/uptrace/bun/driver/sqliteshim v1.2.3
+	github.com/uptrace/bun/extra/bundebug v1.2.3
+	golang.org/x/exp v0.0.0-20231108232855-2478ac86f678
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.4.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	modernc.org/gc/v3 v3.0.0-20240801135723-a856999a2e4a // indirect
+	modernc.org/libc v1.60.1 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/sqlite v1.32.0 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/dchest/captcha"
+	"golang.org/x/exp/rand"
+
+	"github.com/gofiber/fiber/v2"
+
+	"database/sql"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/sqlitedialect"
+	"github.com/uptrace/bun/driver/sqliteshim"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+var (
+	DB         *bun.DB
+	Ctx        = context.Background()
+	captchaDir = "captchas"
+)
+
+type Captcha struct {
+	bun.BaseModel `bun:"table:player,alias:p"`
+
+	ID        uint64    `bun:",pk,autoincrement"`
+	IP        string    `bun:",notnull"`
+	Token     string    `bun:",notnull"`
+	Value     string    `bun:",notnull"`
+	Timestamp time.Time `bun:",notnull,default:current_timestamp"`
+}
+
+type Contact struct {
+	Email   string `json:"email"`
+	Captcha string `json:"captcha"`
+}
+
+func RandomString(length int) (string, error) {
+	randomBytes := make([]byte, length)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+
+	randomString := base64.URLEncoding.EncodeToString(randomBytes)
+
+	return randomString, nil
+}
+
+func deleteOldCaptcha(minutes int) error {
+	threshold := time.Now().Add(-time.Duration(minutes) * time.Minute)
+
+	return filepath.Walk(captchaDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && info.ModTime().Before(threshold) {
+			fmt.Println("Deleting:", path)
+			return os.Remove(path)
+		}
+
+		return nil
+	})
+}
+
+func main() {
+	if err := os.MkdirAll("captchas", os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	sqldb, err := sql.Open(sqliteshim.ShimName, "file::memory:?cache=shared")
+	if err != nil {
+		panic(err)
+	}
+	DB := bun.NewDB(sqldb, sqlitedialect.New())
+
+	file, err := os.Create("bundebug.log")
+	if err != nil {
+		panic(err)
+	}
+
+	DB.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+		bundebug.WithWriter(file),
+	))
+
+	if err := DB.Ping(); err != nil {
+		panic(err)
+	}
+
+	DB.RegisterModel(&Captcha{})
+	if _, err = DB.NewCreateTable().Model(&Captcha{}).Exec(Ctx); err != nil {
+		panic(err)
+	}
+
+	app := fiber.New()
+
+	app.Post("/contact", func(c *fiber.Ctx) error {
+		captchaToken := c.Cookies("captcha_token")
+		ipHash := sha256.Sum256([]byte(c.IP()))
+		ip := fmt.Sprintf("%x", ipHash)
+
+		captchaEntity := Captcha{}
+		getCaptcha := DB.NewSelect().Model(&captchaEntity)
+		getCaptcha.Where("token = ? AND ip = ? AND timestamp > ?", captchaToken, ip, time.Now().Add(-2*time.Minute))
+		if err := getCaptcha.Scan(Ctx); err != nil {
+			return c.SendStatus(404)
+		}
+
+		var contactForm Contact
+		if err := c.BodyParser(&contactForm); err != nil {
+			return c.SendStatus(400)
+		}
+
+		if !captcha.VerifyString(captchaEntity.Value, contactForm.Captcha) {
+			return c.SendStatus(403)
+		}
+
+		return c.SendStatus(200)
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("contact.html")
+	})
+
+	app.Get("/captcha", func(c *fiber.Ctx) error {
+		token, err := RandomString(32)
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		_ = deleteOldCaptcha(2)
+
+		captchaCookie := new(fiber.Cookie)
+		captchaCookie.Name = "captcha_token"
+		captchaCookie.Value = token
+		captchaCookie.Expires = time.Now().Add(2 * time.Minute)
+		captchaCookie.HTTPOnly = true
+		captchaCookie.Secure = true
+		c.Cookie(captchaCookie)
+
+		captchaId := captcha.New()
+
+		ipHash := sha256.Sum256([]byte(c.IP()))
+		ip := fmt.Sprintf("%x", ipHash)
+
+		captchaEntity := &Captcha{
+			IP:    ip,
+			Token: token,
+			Value: captchaId,
+		}
+
+		insertCaptcha := DB.NewInsert().Model(captchaEntity)
+		if _, err = insertCaptcha.Exec(Ctx); err != nil {
+			return c.SendStatus(400)
+		}
+
+		captchaPath := filepath.Join("captchas", captchaId+".png")
+
+		// create file writer
+		file, err := os.Create(captchaPath)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to create captcha file")
+		}
+		defer file.Close()
+
+		// Write the captcha image
+		if err = captcha.WriteImage(file, captchaId, 240, 80); err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to write captcha image")
+		}
+
+		return c.SendFile(captchaPath, true)
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
